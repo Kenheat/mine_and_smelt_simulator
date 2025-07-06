a272abecaeb1df9a05de99abd57ab8833e2eca73
Kenheat <kenneth_mo1@hotmail.com>
2025-07-06 17:04:12 +0200

    feat: help menu, inventory creation, command functionality, function calls

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from items import *
 from actions import *
+from inventory import *
 
 def main():
+    print("Start game.")
+    help_menu()
+
+    valid_commands = ["q", "m", "s", "i"]
+
+    inventory = Inventory()
+
     while True:
         user_input = input(">")
 
+        if user_input not in valid_commands:
+            print("Invalid command.")
+            continue
+
         if user_input == "q":
-            print("Goodbye!")
+            print("Quit game.")
             return
         
         if user_input == "m":
-            mine()
+            ore = mine(Vein())
+            inventory.add_item(ore)
 
         if user_input == "s":
             smelt()
+        
+        if user_input == "i":
+            inventory.show_inventory()
 
 if __name__ == "__main__":
     main()
