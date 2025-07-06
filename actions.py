077293d020bfefeb2b702764c4aaf01b241adf3f
Kenheat <kenneth_mo1@hotmail.com>
2025-07-06 16:59:54 +0200

    feat: help menu, refactor: mine functionality

--- actions.py
+++ actions.py
@@ -1,7 +1,11 @@
 import time
+from items import *
 
-def mine():
-    pass
+def mine(vein):
+    vein_to_mine = vein
+    extract = vein_to_mine.extract()
+    print("Ore extracted.")
+    return extract
 
 def smelt():
     pass
@@ -13,3 +17,11 @@ def animation():
         time.sleep(1)
         print(symbol)
         symbol += "*"
+
+def help_menu():
+    print("")
+    print("q: quit")
+    print("m: mine")
+    print("s: smelt")
+    print("i: inventory")
+    print("")
