5bd99c62d8aeb9f9e1bfdde3dcf1c51b42192185
Kenheat <kenneth_mo1@hotmail.com>
2025-07-06 17:02:46 +0200

    refactor: remove inventory class, Vein functionality

--- items.py
+++ items.py
@@ -1,15 +1,17 @@
 class Vein:
     def __init__(self):
-        pass
+        self.type = "Gold"
+
+    def extract(self):
+        return Ore()
 
 class Ore(Vein):
     def __init__(self):
         super().__init__()
 
+    def __str__(self):
+        return f"{self.type} ore"
+
 class Bar(Ore):
     def __init__(self):
         super().__init__()
-
-class Inventory():
-    def __init__(self):
-        self.items = {}
