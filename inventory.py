926b96ba604948aaf9ef8c8213a6059e1126d7d7
Kenheat <kenneth_mo1@hotmail.com>
2025-07-08 21:36:42 +0200

    feat: take item from inventory. refactor: show inventory.

--- inventory.py
+++ inventory.py
@@ -11,6 +11,16 @@ class Inventory():
         else:
             self.quantities[str(item)] = 1
     
+    def take_item(self):
+        for i in range(len(self.items)):
+            if str(self.items[i]) == "Gold ore":
+                self.quantities["Gold ore"] -= 1
+                return self.items.pop(i)
+    
     def show_inventory(self):
+        if len(self.items) == 0:
+            print("No items in inventory.")
+            return
         for item in self.quantities:
-            print(f"{item} x {self.quantities[item]}")
+            if self.quantities[item] != 0:
+                print(f"{item} x {self.quantities[item]}")
